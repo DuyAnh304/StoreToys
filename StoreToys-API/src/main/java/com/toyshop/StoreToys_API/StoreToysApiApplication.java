43bf359fd9a749f67f294e17bca4bbdff928a9f3
Duy Anh <namelessda123@gmail.com>
2024-08-13 20:21:48 +0700

    Create project

---
+++ StoreToys-API/src/main/java/com/toyshop/StoreToys_API/StoreToysApiApplication.java
@@ -0,0 +1,13 @@
+package com.toyshop.StoreToys_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreToysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreToysApiApplication.class, args);
+	}
+
+}
---
+++ StoreToys-API/src/test/java/com/toyshop/StoreToys_API/StoreToysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyshop.StoreToys_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreToysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
